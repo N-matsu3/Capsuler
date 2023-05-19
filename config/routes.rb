fd2d11046ec798c306ae8207cf9f549cd1f84a00
N-matsu3 <123649194+N-matsu3@users.noreply.github.com>
2023-05-19 16:48:31 +0000

    ake user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,30 @@
 Rails.application.routes.draw do
-  
+
+  namespace :public do
+    get 'users/show'
+    get 'users/edit'
+    get 'users/update'
+  end
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
 }
-  
+
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
 }
+
+scope module: :public do
+#homes
+  root to: "homes#top"
+  get "about" => "homes#about"
+
+#users
+  get "users/my_page" => "users#show"
+
+
+end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
